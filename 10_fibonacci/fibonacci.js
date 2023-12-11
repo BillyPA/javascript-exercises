953653564ec48f08883b7c8180c67382bb01269e
bill <papakosmasbill@gmail.com>
2023-12-11 21:51:47 +0200

    training

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number< 0){
+        return "OOPS"
+    }
+    
+    let firstPrev =1;
+    let secPrev =0;
 
+    for(let i = 2; i<= number; i++){
+        let curNum = firstPrev + secPrev;
+        secPrev = firstPrev;
+        firstPrev = curNum;
+    }
+    return firstPrev;
 };
 
 // Do not edit below this line
